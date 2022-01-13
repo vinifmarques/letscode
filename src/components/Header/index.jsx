7a6528d6c55a294396406628ce35749c395ce4fe
vinnymarx <viiimarques@me.com>
2022-01-13 20:01:37 -0300

    primeiro commit react

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+
+function Header() {
+    return (
+        <header className='container'>
+            <h1 className='title'>Lista de Produtos</h1>
+        </header>
+    );
+}
+export default Header
