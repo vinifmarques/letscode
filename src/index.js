00a7ba19854e3743ee20b2491404fd9930895ace
vinnymarx <viiimarques@me.com>
2022-02-15 20:47:01 -0300

    5 commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App_lifecycle from './App_lifecycle'
-/* import App from './App' */
+import App from './App' 
 
 
 ReactDOM.render(
