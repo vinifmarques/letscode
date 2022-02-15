53b41aed45023a0091f2ef1689e6e94878ac0871
vinnymarx <viiimarques@me.com>
2022-02-15 20:30:51 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,11 @@ import ProductList from './components/ProductList'
 import products from './mock/products'
 import './App.css'
 
-
-const LazyComponentRender = React.lazy( () => import('./components/LazyComponent'))
-
 function App() {
     return (
         <>
-            <Suspense fallback={<div>Carregando...</div>}>
-                <LazyComponent/>
-            </Suspense>
             <Header />
             <ProductList products={products}/>
-
         </>
     )
 }
